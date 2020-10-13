953132ad63b33af7d1e5a8a864c9b272ea6837f5
brageboe <bragebs@gmail.com>
2020-10-13 14:12:25 +0200

    Updated 23-16.py with part b.

--- exercise-7/23-16.py
+++ exercise-7/23-16.py
@@ -10,22 +10,22 @@ Created on Mon Oct 12 15:51:48 2020
 # * Plots phase shifts for both TE and TM
 # * Plots phase difference between TE and TM together with a line (default -3pi/4)
 #   - change picheck to a different multiple of pi/2 to something more suitable depending on your rhomb
-#   
+#
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-theta_critical = np.arcsin(1/1.5)
-N = 128
-theta = np.linspace(theta_critical, np.pi/2, N)
-n1 = 1
-n2 = 1.65
-n = n1 / n2
+theta_critical = np.arcsin(1/1.5)           # critical angle for total internal reflection
+N = 128                                     # number of data points
+theta = np.linspace(theta_critical, np.pi/2, N) # angle of incidence
+n1 = 1                                      # refractive index of ambient
+n2 = 1.65                                   # refractive index of fresnel rhomb
+n = n1 / n2                                 # relative refractive index
 
 print("Critical angle = ", theta_critical * 180 / np.pi)
-C1 = np.sqrt(np.sin(theta)**2 - n**2) 
 
-# equations (23-38) and (23-39) in PPP
+C1 = np.sqrt(np.sin(theta)**2 - n**2)
+# Phase shift equations (23-38) and (23-39) in PPP
 phase_TM = -2 * np.arctan( C1 / (n**2 * np.cos(theta)) ) + np.pi
 phase_TE = -2 * np.arctan( C1 / np.cos(theta) )
 
@@ -53,8 +53,16 @@ plt.ylabel(r"Phase shift $\phi$ [rad]")
 plt.legend()
 plt.show()
 
-theta_apex = theta[idx]
-print("Apex angle(s) for a Fresnel Rhomb with refractive index", n2, "is: ", theta_apex * 180 / np.pi)
+theta_apex = theta[idx] # the angle for which two internal reflections in a Fresnel rhomb produce a relative phase shift of pi/2 (i.e. circular pol.)
+print("Apex angle for a Fresnel Rhomb with refractive index", n2, "is:", theta_apex * 180 / np.pi)
 
 # 23-16 b)
-# What is the phase difference between TE and TM (after both reflections inside the rhomb) when the angle is 5% above/below the apex angle?
+# What is the phase difference between TE and TM (after BOTH reflections inside the rhomb) when the angle is 5% above/below the apex angle?
+apex_deviated = [0.95 * theta_apex, 1.05 * theta_apex]
+
+C2 = np.sqrt(np.sin(apex_deviated)**2 - n**2)
+# multiply phase shifts by 2, due to two internal reflections inside rhomb:
+phase_TM_deviated = -4 * np.arctan( C2 / (n**2 * np.cos(apex_deviated)) ) + np.pi
+phase_TE_deviated = -4 * np.arctan( C2 / np.cos(apex_deviated) )
+phase_difference_deviated = phase_TE_deviated - phase_TM_deviated
+print("Phase difference between TM and TE modes after both internal reflections, when the apex angle is 5% below/above correct apex angle:", phase_difference_deviated / np.pi, "* pi.")
